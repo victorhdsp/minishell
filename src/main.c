38850f8b9f516046ca5ff855e6b2f77cd6646d34
vide-sou <victorhugods.pereira@gmail.com>
2025-03-24 16:01:07 -0300

    implements parser service

--- src/main.c
+++ src/main.c
@@ -14,6 +14,34 @@ int     main(void)
     //char *input[] = {"|", ">", "<", "&&", "||", NULL};
     //char *input[] = {"comando1", "|", "comando2", ">", "output.txt", "=", "e", NULL};
     //lexer(input);
+
+    /*char **input = word_breaker("comando argumento1 argumento2");
+    t_sentence_item *cmds = lexer(input);
+    int result = ft_parser(&cmds);
     
+    int     index;
+    index = 0;
+    printf("input!\n");
+    while (input[index])
+    {
+        printf("    %s\n", input[index]);
+        index++;
+    }
+    index = 0;
+    printf("lexer!\n");
+    while (cmds[index].value)
+    {
+        printf("    value: %s\n", cmds[index].value);
+        printf("    type: %d\n", cmds[index].type);
+        printf("    fn: %d\n", cmds[index].fn);
+        index++;
+    }
+    printf("parser!\n");
+    printf("    result: %d\n", result);*/
+    char *cmd[] = {"(", "comando1", "&&", "comando2", "||", "comando3", NULL};
+    t_sentence_item *input = lexer((char **)cmd);
+
+    int             expected = 2;
+    int             result = ft_parser((t_sentence_item **)&input);
     return (1);
 }
