dbfff7720797640680fa3d4f8cab34ebbc592c32
vide-sou <victorhugods.pereira@gmail.com>
2025-03-24 10:21:32 -0300

    mudança do retorno de []quando é vazio para nulo

--- src/main.c
+++ src/main.c
@@ -2,10 +2,12 @@
 
 int     main(void)
 {
-    word_breaker("comando argumento1 argumento2");
-    word_breaker("comando \"argumento com espaços\" argumento2");
-    word_breaker("comando1|comando2 > output.txt=e");
-    word_breaker("|>>|>|<||");
-    word_breaker("comando \"argumento não fechado");
+    //word_breaker("comando argumento1 argumento2");
+    //word_breaker("comando \"argumento com espaços\" argumento2");
+    //word_breaker("comando1|comando2 > output.txt=e");
+    //word_breaker("|>>|>|<||");
+    //word_breaker("comando \"argumento não fechado");
+    word_breaker("");
+    word_breaker(" ");
     return (1);
 }
