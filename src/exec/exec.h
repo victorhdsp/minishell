79b3b5b09001a32ed71689ccaab421c99d6830a9
vide-sou <victorhugods.pereira@gmail.com>
2025-04-09 15:36:48 -0300

    static heredoc

--- src/exec/exec.h
+++ src/exec/exec.h
@@ -6,7 +6,7 @@
 /*   By: vide-sou <vide-sou@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 13:50:41 by vide-sou          #+#    #+#             */
-/*   Updated: 2025/04/03 14:08:36 by vide-sou         ###   ########.fr       */
+/*   Updated: 2025/04/08 16:32:16 by vide-sou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@
 # include <fcntl.h>
 # include <sys/stat.h>
 # include <sys/wait.h>
-
-int		ft_heredoc(char *exit);
 
 void	ft_prepare_exec(t_sentence *sentence);
 void	ft_use_redirects(t_sentence *sentence);
