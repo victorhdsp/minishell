b45cb9f8c0a9ba8be792426c43ee63e6892acda9
vide-sou <victorhugods.pereira@gmail.com>
2025-04-14 16:26:53 -0300

    Create folder and init folders

--- __test/src/prepare_exec.test.c
+++ __test/src/prepare_exec.test.c
@@ -11,7 +11,7 @@ void suite_teardown(void) {
 
 // Test case: Redirect output to a file
 Test(prepare_exec, test_redirect_output_to_file) {
-    char      *filename = "/usr/src/app/local/tmp/test_output.txt";
+    char      *filename = "/usr/src/app/__test/local/tmp/test_output.txt";
     t_lexer_item    input[] = {
 		{.value = ">", .type = type_outfile, .fn = fn_output},
 		{.value = filename, .type = type_word, .fn = fn_null},
@@ -27,7 +27,7 @@ Test(prepare_exec, test_redirect_output_to_file) {
 // Test case: Redirect input from a file
 Test(prepare_exec, test_redirect_input_from_file)
 {
-    char *filename = "/usr/src/app/local/tmp/test_input.txt";
+    char *filename = "/usr/src/app/__test/local/tmp/test_input.txt";
     int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     write(fd, "test input", 10);
     t_lexer_item input[] = {
@@ -45,7 +45,7 @@ Test(prepare_exec, test_redirect_input_from_file)
 // Test case: Invalid file descriptor
 Test(prepare_exec, test_invalid_file_descriptor, .init=suite_setup, .fini=suite_teardown, .exit_code = 1)
 {
-    char *filename = "/usr/src/app/local/tmp/test_invalid_fd.txt";
+    char *filename = "/usr/src/app/__test/local/tmp/test_invalid_fd.txt";
     t_lexer_item input[] = {
         {.value = "<", .type = type_infile, .fn = fn_input},
         {.value = filename, .type = type_word, .fn = fn_null},
@@ -71,7 +71,7 @@ Test(prepare_exec, test_invalid_file_path, .init=suite_setup, .fini=suite_teardo
 
 // Test case: Redirect append to a file
 Test(prepare_exec, test_redirect_append_to_file) {
-    char      *filename = "/usr/src/app/local/tmp/test_append.txt";
+    char      *filename = "/usr/src/app/__test/local/tmp/test_append.txt";
     t_lexer_item    input[] = {
 		{.value = ">>", .type = type_outfile, .fn = fn_append},
 		{.value = filename, .type = type_word, .fn = fn_null},
@@ -114,7 +114,7 @@ Test(prepare_exec, test_redirect_input_from_invalid_file, .init=suite_setup, .fi
 
 // Test case: Create HEREDOC file
 Test(prepare_exec, test_create_heredoc_file) {
-    char *filename = "/usr/src/app/__test/local/heredoc/heredoc_0";
+    char *filename = "/usr/src/app/__test/local/heredoc/heredoc_1";
     char *heredoc_content = "test heredoc content\n";
     char *heredoc_delimiter = "EOF";
     t_lexer_item input[] = {
@@ -129,9 +129,9 @@ Test(prepare_exec, test_create_heredoc_file) {
         ft_use_redirects(sentence);
         exit(0);
     }
-    usleep(100);
     write(0, heredoc_content, strlen(heredoc_content));
     write(0, heredoc_delimiter, strlen(heredoc_delimiter));
+    usleep(100);
     int result = access(filename, F_OK);
     cr_assert(result == 0, "File not created");
     remove(filename);
