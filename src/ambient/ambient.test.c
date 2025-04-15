d7124ba9813381b62a0b07c8a81f0a11956771c0
vide-sou <victorhugods.pereira@gmail.com>
2025-04-15 17:00:08 -0300

    Testes para variaveis do sistema.

---
+++ src/ambient/ambient.test.c
@@ -0,0 +1,52 @@
+#include "../../__test/utils.h"
+#include "./ambient.h"
+
+
+void suite_setup(void) {
+    char		*env[] = {NULL};
+	t_system	new_sys;
+
+	new_sys.name = "test";
+	new_sys.last_exit_status = 0;
+	new_sys.env = env;
+	get_system(&new_sys);
+}
+
+Test(ambient, set_system_info, .init = suite_setup)
+{
+	t_system	expected_sys;
+
+	expected_sys = get_system(NULL);
+	cr_assert_str_eq(expected_sys.name, "test",
+                    "Nome do sistema esperado está incorreto: esperado {test}, recebido {%s}",
+					expected_sys.name);
+	cr_assert_eq(expected_sys.last_exit_status, 0,
+                    "Ultimo exit do sistema esperado está incorreto: esperado {test}, recebido {%d}",
+					expected_sys.last_exit_status);
+	cr_assert_eq(expected_sys.env[0], NULL,
+                    "Env do sistema esperado está incorreto: esperado {(null)}, recebido {%s}",
+					expected_sys.env[0]);
+}
+
+Test(ambient, change_last_exit, .init = suite_setup)
+{
+	t_system	expected_sys;
+
+	set_system_exit_status(1);
+	expected_sys = get_system(NULL);
+	cr_assert_eq(expected_sys.last_exit_status, 1,
+                    "Ultimo exit do sistema esperado está incorreto: esperado {1}, recebido {%d}",
+					expected_sys.last_exit_status);
+}
+
+Test(ambient, change_enb, .init = suite_setup)
+{
+	t_system	expected_sys;
+	char		*env[] = {"test=env", NULL};
+
+	set_system_env(env);
+	expected_sys = get_system(NULL);
+	cr_assert_str_eq(expected_sys.env[0], "test=env",
+                    "Env do sistema esperado está incorreto: esperado {(null)}, recebido {%s}",
+					expected_sys.env[0]);
+}
