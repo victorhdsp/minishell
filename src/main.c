d6903543c84b7327d3307580f1dfd3488ad4f0d0
vide-sou <victorhugods.pereira@gmail.com>
2025-04-18 04:47:34 -0300

    add variable traitement

--- src/main.c
+++ src/main.c
@@ -1,53 +1,17 @@
-/*#include "./minishell.h"
+#include "./minishell.h"
+#include "./traitement/traitement.h"
 
 int main(int ac, char **av, char **env)
 {
-    t_my_env *my_env = get_env(env);
-    char *input[] = {"echo", NULL};
+    char *input = "\"comando\" \"$PATH\"";
+    char **breaked = word_breaker(input);
+    t_system system;
 
-    char    *envv[] = {
-        "GDMSESSION=ubuntu",
-        "DISPLAY=:0",
-        "SHLVL=1",
-        "OLDPWD=/home/rpassos-",
-        "MAIL=rpassos-@student.42.rio",
-        NULL
-        };
-    t_my_env *my_env2 = get_env(envv);
-
-    char    *envv2[] = {
-        "unset",
-        "GDMSESSION",
-        "MAIL",
-        "OLDPWDA",
-        NULL
-        };
-
-
-    if (av[1] && (ft_strcmp(av[1], "env") == 0))
-        print_env(my_env2);
-    if (av[1] && (ft_strcmp(av[1], "export") == 0))
-    {   
-        ft_export(&my_env, av);
-        //ft_export(&my_env, param);
-        //print_env(my_env);
-    }
-    if (av[1] && (ft_strcmp(av[1], "unset") == 0))
-    {   
-        print_env(my_env2);
-        ft_unset(&my_env2, envv2);
-        printf("==========\n");
-        
-        print_env(my_env2);
-        //order(my_env);
-    }
-    if (av[1] && (ft_strcmp(av[1], "echo") == 0)) 
- {
-    //       ft_echo(ac, av);  
-        ft_echo(1, input);
+    system.name = "victor";
+    system.last_exit_status = 0;
+    system.env = env;
+    get_system(&system);
+    t_lexer_item *lexed = lexer(breaked);
+    variable_traitement(lexed);
+    quote_traitement(lexed);
 }
-    ft_lstclear_env(&my_env); //lembrar de limpar a env na main pra nao dar leak
-    
-    return (ac);
-}
-*/
---
+++ src/traitement/variable.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   variable.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vide-sou <vide-sou@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 01:14:22 by vide-sou          #+#    #+#             */
+/*   Updated: 2025/04/18 03:04:33 by vide-sou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./traitement.h"
+
+static char     *get_new_value(char *key, char *old_value)
+{
+    int     index;
+    char    *var_name;
+    char    *tmp;
+    char    *tmp2;
+    char    *result;
+
+    index = 0;
+    while (key[index] && (ft_isalnum(key[index]) || key[index] == '_'))
+        index++;
+    var_name = ft_substr(key, 0, index);
+    tmp = ft_substr(old_value, 0, key - old_value - 1);
+    tmp2 = get_system_env(var_name);
+    result = ft_strjoin(tmp, tmp2);
+    free(tmp);
+    free(tmp2);
+    tmp = result;
+    tmp2 = ft_strdup(key + index);
+    result = ft_strjoin(tmp, tmp2);
+    free(tmp);
+    free(tmp2);
+    return (result);
+}
+
+void    variable_traitement(t_lexer_item *args)
+{
+    int     index;
+    char    *str;
+    char    *key;
+    char    *tmp;
+    
+
+    index = 0;
+    while (args[index].value)
+    {
+        str = args[index].value;
+        if (str[0] != '\'')
+        {
+            if (str[0] == '\"')
+                str++;
+            key = ft_memchr(str, '$', ft_strlen(str));
+            if (key)
+            {
+                tmp = get_new_value(key + 1, args[index].value);
+                free(args[index].value);
+                args[index].value = tmp;
+            }
+        }
+        index++;
+    }
+}
