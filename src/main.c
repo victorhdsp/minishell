27c2072f07a698d27c533b6dd9f90a0bb0a3c607
vide-sou <victorhugods.pereira@gmail.com>
2025-04-18 07:23:34 -0300

    code to test quote and variable traitement

--- src/main.c
+++ src/main.c
@@ -3,15 +3,21 @@
 
 int main(int ac, char **av, char **env)
 {
-    char *input = "\"comando\" \"$PATH\"";
+    /*char *input = "\"comando\" \"$PATH\"";
     char **breaked = word_breaker(input);
-    t_system system;
 
+    t_lexer_item *lexed = lexer(breaked);
+    variable_traitement(lexed);
+    quote_traitement(lexed);*/
+    t_system system;
     system.name = "victor";
     system.last_exit_status = 0;
     system.env = env;
     get_system(&system);
+
+    char *input = strdup("");
+    char *expected = "";
+    char **breaked = word_breaker(input);
     t_lexer_item *lexed = lexer(breaked);
-    variable_traitement(lexed);
     quote_traitement(lexed);
 }
