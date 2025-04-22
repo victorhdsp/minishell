80f3afe1ffa2fe710c217cff000854b23db47246
vide-sou <victorhugods.pereira@gmail.com>
2025-04-22 10:12:50 -0300

    add program flow

---
+++ src/flow/program.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   program.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vide-sou <vide-sou@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 08:46:27 by vide-sou          #+#    #+#             */
+/*   Updated: 2025/04/22 10:05:06 by vide-sou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../tokenizer/tokenizer.h"
+#include "../traitement/traitement.h"
+#include "../exec/exec.h"
+#include "./flow.h"
+
+int minishell_flow(char *cmd)
+{
+    char            **splited_cmd;
+    t_lexer_item    *lexed_cmd;
+    t_sentence      *sentence_cmd;
+    
+    splited_cmd = word_breaker(cmd);
+    lexed_cmd = lexer(splited_cmd);
+    set_system_exit_status(ft_parser(&lexed_cmd));
+    variable_traitement(lexed_cmd);
+    quote_traitement(lexed_cmd);
+    sentence_cmd = create_pipes(lexed_cmd);
+    exec_command(sentence_cmd);
+}
--- src/main.c
+++ src/main.c
@@ -1,23 +1,16 @@
 #include "./minishell.h"
 #include "./traitement/traitement.h"
+#include <readline/readline.h>
 
 int main(int ac, char **av, char **env)
 {
-    /*char *input = "\"comando\" \"$PATH\"";
-    char **breaked = word_breaker(input);
+    char    *cmd;
 
-    t_lexer_item *lexed = lexer(breaked);
-    variable_traitement(lexed);
-    quote_traitement(lexed);*/
-    t_system system;
-    system.name = "victor";
-    system.last_exit_status = 0;
-    system.env = env;
-    get_system(&system);
-
-    char *input = strdup("");
-    char *expected = "";
-    char **breaked = word_breaker(input);
-    t_lexer_item *lexed = lexer(breaked);
-    quote_traitement(lexed);
+    (void) ac;
+    (void) av;
+    system_flow(env, "minishell");
+    cmd = readline("minishel: ");
+    //cmd = ft_strdup("ls");
+    minishell_flow(cmd);
+    return (0);
 }
