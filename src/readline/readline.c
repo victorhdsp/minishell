477cc8b93248077215a08da410a92cff9c238360
rpassos- <renatopassos.adv@gmail.com>
2025-05-05 17:06:30 -0300

    fix: readline prompt

--- src/readline/readline.c
+++ src/readline/readline.c
@@ -6,21 +6,39 @@
 /*   By: rpassos- <rpassos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 10:53:44 by rpassos-          #+#    #+#             */
-/*   Updated: 2025/04/24 12:11:52 by rpassos-         ###   ########.fr       */
+/*   Updated: 2025/05/05 17:04:28 by rpassos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "my_readline.h"
 #include "../signals_handler/signals_handler.h"
+#include "../ambient/ambient.h"
+
+char	*set_prompt()
+{
+	char	*pwd;
+	char	*user;
+	char	*result;
+
+	user = get_system_env("USER");
+	pwd = get_system_env("PWD");
+	result = ft_strnstr(pwd, user, ft_strlen(pwd));
+	if (!result)
+		return (pwd);
+	return (result);
+}
 
 void	read_entrys(void)
 {
-	char	*line;
+	char		*line;
+	t_system	system;
 	
 	signal_handler();
 	while(1)
 	{
-		line = readline("minishell$: ");
+		system = get_system(NULL);
+		set_prompt();
+		line = readline(system.name);
 		if (line == NULL)
 		{
 			printf("exit\n");
