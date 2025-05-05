f9ce932b9f546e32800f662f32184fe41394e454
vide-sou <victorhugods.pereira@gmail.com>
2025-05-05 16:05:38 -0300

    update env on ambient

--- src/builtin/exec.c
+++ src/builtin/exec.c
@@ -6,7 +6,7 @@
 /*   By: vide-sou <vide-sou@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 11:15:31 by vide-sou          #+#    #+#             */
-/*   Updated: 2025/04/23 13:38:30 by vide-sou         ###   ########.fr       */
+/*   Updated: 2025/05/05 16:04:34 by vide-sou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,38 +20,53 @@ int export_builtin(char **args)
 {
     t_system    system;
     t_my_env    *list_env;
+    int         result;
+    char        **arr_env;
 
     system = get_system(NULL);
     list_env = get_env(system.env);
-    return (ft_export(&list_env, args));
+    result = ft_export(&list_env, args);
+    set_system_env(arr_env);
+    return (result);
 }
 
 int unset_builtin(char **args)
 {
     t_system    system;
     t_my_env    *list_env;
+    int         result;
+    char        **arr_env;
 
     system = get_system(NULL);
     list_env = get_env(system.env);
-    return (ft_unset(&list_env, args));
+    result = ft_unset(&list_env, args);
+    arr_env = get_env_arr(list_env);
+    set_system_env(arr_env);
+    return (result);
 }
 
 int env_builtin()
 {
     t_system    system;
     t_my_env    *list_env;
+    int         result;
 
     system = get_system(NULL);
     list_env = get_env(system.env);
-    return (print_env(list_env));
+    result = print_env(list_env);
+    return (result);
 }
 
 int cd_builtin(char **args)
 {
     t_system    system;
     t_my_env    *list_env;
+    int         result;
+    char        **arr_env;
 
     system = get_system(NULL);
     list_env = get_env(system.env);
-    return (ft_cd(&list_env, args));
+    result = ft_cd(&list_env, args);
+    set_system_env(arr_env);
+    return (result);
 }
