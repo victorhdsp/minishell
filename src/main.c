def62800b8e5389d25d54ecf8bf5805decef805d
vide-sou <victorhugods.pereira@gmail.com>
2025-05-12 10:58:47 -0300

    refactor: remove unused vars

--- src/main.c
+++ src/main.c
@@ -6,20 +6,17 @@
 /*   By: vide-sou <vide-sou@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 10:50:40 by vide-sou          #+#    #+#             */
-/*   Updated: 2025/05/12 10:50:54 by vide-sou         ###   ########.fr       */
+/*   Updated: 2025/05/12 10:58:05 by vide-sou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./flow/flow.h"
 #include "./minishell.h"
 #include "./traitement/traitement.h"
-#include <readline/readline.h>
+#include "./readline/my_readline.h"
 
 int	main(int ac, char **av, char **env)
 {
-	char		*cmd;
-	t_system	system;
-
 	(void)ac;
 	(void)av;
 	system_flow(env, "minishell");
