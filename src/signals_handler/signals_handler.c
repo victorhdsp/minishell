8044b63b88b0c8b66cdf6f83d70c8f95d1c14144
vide-sou <victorhugods.pereira@gmail.com>
2025-06-03 16:27:19 -0300

    fix: signal error in multiples shells

--- src/signals_handler/signals_handler.c
+++ src/signals_handler/signals_handler.c
@@ -6,7 +6,7 @@
 /*   By: vide-sou <vide-sou@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 11:18:26 by rpassos-          #+#    #+#             */
-/*   Updated: 2025/05/16 14:52:10 by vide-sou         ###   ########.fr       */
+/*   Updated: 2025/06/03 16:26:18 by vide-sou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ void	handler_ctrl(int signal, siginfo_t *info, void *notused)
 	(void)notused;
 	if (signal == SIGINT)
 	{
-		printf("\n");
-		rl_replace_line("", 0);
-		rl_on_new_line();
 		if (info->si_uid)
+		{
+			printf("\n");
+			rl_on_new_line();
+			rl_replace_line("", 0);
 			rl_redisplay();
+		}
 		set_system_exit_status(130);
 	}
 	else if (signal == SIGQUIT)
@@ -34,7 +36,7 @@ void	signal_handler(void)
 
 	sigemptyset(&sa.sa_mask);
 	sa.sa_sigaction = handler_ctrl;
-	sa.sa_flags = SA_SIGINFO;
+	sa.sa_flags = SA_SIGINFO | SA_RESTART;
 	if (sigaction(SIGINT, &sa, NULL) == -1)
 		perror("SIGINT error");
 	if (sigaction(SIGQUIT, &sa, NULL) == -1)
